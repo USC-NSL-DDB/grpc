63831862e0a9ca374f375317b5e0ddaebef069b4
Yibo Yan <yiboyan@usc.edu>
2024-11-25 20:01:46 -0700

    mod: setup script for permission

--- setup.sh
+++ setup.sh
@@ -4,27 +4,27 @@ sudo apt-get update
 sudo apt-get install -y build-essential gcc autoconf libtool pkg-config make cmake cmake-gui cmake-curses-gui git
 
 if [ "$1" = "setup-broker" ]; then
-      sudo apt-add-repository -y ppa:mosquitto-dev/mosquitto-ppa
-      sudo apt-get update
-      sudo apt-get install -y libc-ares-dev libssl-dev mosquitto # install mosquitto broker directly
+  sudo apt-add-repository -y ppa:mosquitto-dev/mosquitto-ppa
+  sudo apt-get update
+  sudo apt-get install -y libc-ares-dev libssl-dev mosquitto # install mosquitto broker directly
 
-      # disable auto-start mosquitto service
-      sudo systemctl disable mosquitto.service
-      sudo systemctl stop mosquitto.service
-      # hack cleanup if any instance is running
-      sudo pkill -9 mosquitto
+  # disable auto-start mosquitto service
+  sudo systemctl disable mosquitto.service
+  sudo systemctl stop mosquitto.service
+  # hack cleanup if any instance is running
+  sudo pkill -9 mosquitto
 
-      TMP_FOLDER="/tmp/mosquitto"
+  TMP_FOLDER="/tmp/mosquitto"
 
-      rm -rf $TMP_FOLDER
-      mkdir -p $TMP_FOLDER
-      pushd $TMP_FOLDER
-      git clone https://github.com/eclipse/paho.mqtt.c.git
-      cd paho.mqtt.c
-      make -j$(nproc)
-      sudo make uninstall # clean up first
-      sudo make install   # install mosquitto c lib
-      popd
+  rm -rf $TMP_FOLDER
+  mkdir -p $TMP_FOLDER
+  pushd $TMP_FOLDER
+  git clone https://github.com/eclipse/paho.mqtt.c.git
+  cd paho.mqtt.c
+  make -j$(nproc)
+  sudo make uninstall # clean up first
+  sudo make install   # install mosquitto c lib
+  popd
 fi
 
 # git submodule update --init
@@ -39,8 +39,9 @@ export PATH="$MY_INSTALL_DIR/bin:$PATH"
 mkdir -p cmake/build
 pushd cmake/build
 cmake -DgRPC_INSTALL=ON \
-      -DgRPC_BUILD_TESTS=OFF \
-      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      ../..
+  -DgRPC_BUILD_TESTS=OFF \
+  -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+  -DCMAKE_C_COMPILER=gcc-13 -DCMAKE_CXX_COMPILER=g++-13 \
+  ../..
 make -j$(nproc)
-make install
+sudo make install
